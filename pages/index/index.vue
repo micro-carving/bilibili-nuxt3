4fabb9297f1af14597b73f9cb77cf20f946c4702
olinonee <olinone666@gmail.com>
2024-07-24 23:44:02 +0800

    feat: add nuxt NuxtLink test demo.

---
+++ pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<h1>这是首页</h1>
+		<VanButton type="primary">主要按钮</VanButton>
+		<VanButton type="danger">信息按钮</VanButton>
+		<van-cell
+			title="展示弹出层"
+			is-link
+			@click="showPopup"
+		/>
+		<van-popup
+			v-model:show="show"
+			:style="{ padding: '64px' }"
+		>
+			内容
+		</van-popup>
+	</div>
+</template>
+
+<script lang="ts" setup>
+const show = ref(false);
+const showPopup = () => {
+	show.value = true;
+};
+</script>
+
+<style></style>
---
+++ pages/video/[id].vue
@@ -0,0 +1,9 @@
+<template>
+	<div>
+		<h2>这是视频页</h2>
+	</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
